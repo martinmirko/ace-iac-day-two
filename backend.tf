c28b47b4fa61de4baed68629cab691501e053aa9
martinmirko <80984229+martinmirko@users.noreply.github.com>
2024-01-12 16:39:25 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "martin-mirko-netops"
+    organization = "martin-mirko-netops"
     workspaces {
       name = "ace-iac-day-two"
     }
